57d4a0e885b3bfeaee1788669a7d4984cd06096c
K A Shaik <khadeer@ks-macbook-pro.home>
2020-04-23 14:43:26 +0100

    implemented post, put and delete

--- api/src/main/java/com/kdrtut/controller/BooksController.java
+++ api/src/main/java/com/kdrtut/controller/BooksController.java
@@ -2,9 +2,7 @@ package com.kdrtut.controller;
 
 import com.kdrtut.service.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,5 +21,20 @@ public class BooksController {
     @GetMapping(value = "/books/{bookid}")
     public Books getBook(@PathVariable int bookid) {
         return booksService.getBookById(bookid);
+    }
+
+    @PostMapping(value ="/books")
+    public List<Books> addBook(@RequestBody Books book) {
+        return booksService.addNewBook(book);
+    }
+
+    @PutMapping(value = "/books/{bookId}")
+    public Books updateBook(@RequestBody Books book, @PathVariable int bookId) {
+        return booksService.updateBook(book, bookId);
+    }
+
+    @DeleteMapping(value = "/books/{bookId}")
+    public void deleteBook(@PathVariable int bookId) {
+        booksService.deleteBook(bookId);
     }
 }
--- api/src/main/java/com/kdrtut/service/BooksService.java
+++ api/src/main/java/com/kdrtut/service/BooksService.java
@@ -3,16 +3,19 @@ package com.kdrtut.service;
 import com.kdrtut.controller.Books;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BooksService {
 
-    private List<Books> books = Arrays.asList(
-            new Books(1, "Book Amazon", 8.89),
-            new Books(2, "Java Fundamentals", 7.99),
-            new Books(3, "Spring boot cook book", 11.99)
+    private List<Books> books = new ArrayList<>(
+            Arrays.asList(
+                    new Books(1, "Book Amazon", 8.89),
+                    new Books(2, "Java Fundamentals", 7.99),
+                    new Books(3, "Spring boot cook book", 11.99)
+            )
     );
 
     public List<Books> getBooks() {
@@ -21,5 +24,29 @@ public class BooksService {
 
     public Books getBookById(int bookId) {
         return books.stream().filter(book -> book.getBookId() == bookId).findFirst().get();
+    }
+
+    public List<Books> addNewBook(Books book) {
+        books.add(book);
+        return books;
+    }
+
+    public Books updateBook(Books book, int bookId) {
+        for(int i = 0; i < books.size(); i++) {
+            Books thisBook = this.books.get(i);
+            if(thisBook.getBookId() == bookId) {
+                books.set(i, book);
+            }
+        }
+        return books.stream().filter(bk -> bk.getBookId() == bookId).findFirst().get();
+    }
+
+    public void deleteBook(int bookId) {
+        for(int i = 0; i < books.size(); i++) {
+            Books thisBook = this.books.get(i);
+            if(thisBook.getBookId() == bookId) {
+                books.remove(i);
+            }
+        }
     }
 }
